db20d7528b72e93e994a063104d8d80e220c1037
Robert deLaguna <rjdelaguna@yahoo.com>
2023-06-01 14:08:46 -0700

    Add places

--- friends.rb
+++ friends.rb
@@ -1,4 +1,8 @@
-array = ["will", "ben", "cait", "john", "rick"]
-puts array[3]
-puts array.last
-puts array.first
+# array = ["will", "ben", "cait", "john", "rick"]
+# puts array[3]
+# puts array.last
+# puts array.first
+
+places = ["canada", "mexico", "france", "spain", "uk"]
+places.pop
+puts places
